d50e30aa5bfe24a40d55fba68b4d7005f46a0b52
unknown <mosharafctg111@gmail.com>
2023-01-06 23:33:57 +0600

    added the title

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 import Navber from './Navber/Navber'
 import AllNews from './AllNews'
 import Banner from './Banner'
 import Footer from './Navber/Footer'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <div>
+      <Head><title>Dummy News</title></Head>
     <Navber></Navber>
     <Banner></Banner>
     <div className='max-w-[1280px] mx-auto'>
     <AllNews></AllNews>
     </div>
     <Footer></Footer>
-    </>
+    </div>
   )
 }
