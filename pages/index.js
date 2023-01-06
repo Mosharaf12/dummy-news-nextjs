1649247653cfc810e0ec0a6e529fb9cf6f567bbe
unknown <mosharafctg111@gmail.com>
2023-01-06 15:54:06 +0600

    added the bg images

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Navber from './Navber/Navber'
 import AllNews from './AllNews'
+import Banner from './Banner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,7 @@ export default function Home() {
   return (
     <>
     <Navber></Navber>
+    <Banner></Banner>
     <div className='max-w-[1280px] mx-auto'>
     <AllNews></AllNews>
     </div>
